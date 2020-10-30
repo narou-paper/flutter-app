3735585f226a2d99ae01053f3c57a1d959566589
sanoto <kmn3rei@gmail.com>
2020-10-31 00:45:09 +0900

    [add] 各テーブルを書き終えた

--- lib/model/db.dart
+++ lib/model/db.dart
@@ -6,26 +6,19 @@ import 'dart:io';
 
 // part 'db.g.dart';
 
-enum Priority {
-  must,
-  should,
-  maybe,
-  notHaveTo,
-}
-
 class Writers extends Table {
   IntColumn get id => integer().autoIncrement()();
-  TextColumn get name => text()();
-  TextColumn get ruby => text()();
+  TextColumn get name => text().withLength(min: 1, max: 100)();
+  TextColumn get ruby => text().withLength(min: 1, max: 100)();
 }
 
-class Novel extends Table {
-  TextColumn get ncode => text()();
+class Novels extends Table {
+  TextColumn get ncode => text().withLength(min: 1, max: 10)();
   IntColumn get ncodeInt => integer().nullable()();
-  TextColumn get title => text()();
-  TextColumn get writerNickname => text()();
+  TextColumn get title => text().withLength(min: 1, max: 500)();
+  TextColumn get writerNickname => text().withLength(min: 1, max: 100)();
   IntColumn get writer => integer()();
-  TextColumn get story => text()();
+  TextColumn get story => text().nullable()();
   BoolColumn get isSerial => boolean()();
   IntColumn get maxEpisodeNum => integer()();
 
@@ -33,34 +26,115 @@ class Novel extends Table {
   Set<Column> get primaryKey => {ncode};
 }
 
-class Todos extends Table {
+class Chapters extends Table {
   IntColumn get id => integer().autoIncrement()();
-  TextColumn get title => text().withLength(min: 1, max: 30)();
-  TextColumn get body => text().nullable()();
-  DateTimeColumn get dueDate => dateTime().nullable()();
-  IntColumn get priority => intEnum<Priority>()();
-  IntColumn get category => integer().nullable()();
+  IntColumn get novel => integer()();
+  TextColumn get name => text().withLength(min: 1, max: 500)();
 }
 
-@DataClassName('Category')
-class Categories extends Table {
+class Episodes extends Table {
   IntColumn get id => integer().autoIncrement()();
-  TextColumn get description => text()();
+  IntColumn get novel => integer()();
+  IntColumn get number => integer()();
+  IntColumn get chapter => integer().nullable()();
+  TextColumn get title => text().withLength(min: 1, max: 500).nullable()();
+  TextColumn get foreword => text().nullable()();
+  TextColumn get body => text()();
+  TextColumn get afterword => text().nullable()();
+  DateTimeColumn get postedAt => dateTime().nullable()();
+  DateTimeColumn get fixedAt => dateTime().nullable()();
+  IntColumn get illustCount => integer().withDefault(const Constant(0))();
 }
 
-class Schedules extends Table {
-  DateTimeColumn get time => dateTime()();
-
-  @override
-  Set<Column> get primaryKey => {time};
+class Illusts extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get uniqueId => text().withLength(min: 1, max: 20)();
+  IntColumn get episode => integer()();
+  TextColumn get title => text().withLength(min: 1, max: 100)();
+  TextColumn get filePath => text().withLength(min: 1, max: 200)();
 }
 
-class Reminders extends Table {
+class KeyWords extends Table {
   IntColumn get id => integer().autoIncrement()();
-  IntColumn get todo => integer()();
-  IntColumn get schedule => integer()();
-  BoolColumn get dailyRepeat => boolean()();
-  BoolColumn get weeklyRepeat => boolean()();
+  TextColumn get name => text().withLength(min: 1, max: 100)();
+}
+
+enum RequiredKeyWord {
+  rating15,
+  boysLove,
+  girlsLove,
+  dangerous,
+  worldReincarnation,
+  worldTransfer,
+}
+
+class NovelDetails extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  IntColumn get novel => integer()();
+  IntColumn get bigGenre => intEnum<BigGenre>()();
+  IntColumn get genre => intEnum<Genre>()();
+  DateTimeColumn get firstPublicationDate => dateTime()();
+  DateTimeColumn get lastPublicationDate => dateTime()();
+  IntColumn get length => integer()();
+  BoolColumn get isStop => boolean()();
+  IntColumn get writerDevice => intEnum<WriterDevice>()();
+  IntColumn get totalPoint => integer()();
+  IntColumn get dailyPoint => integer()();
+  IntColumn get weeklyRatingPoint => integer()();
+  IntColumn get monthlyRatingPoint => integer()();
+  IntColumn get quarterRatingPoint => integer()();
+  IntColumn get yearlyRatingPoint => integer()();
+  IntColumn get bookmarkNum => integer()();
+  IntColumn get impressionNum => integer()();
+  IntColumn get reviewNum => integer()();
+  IntColumn get allPoint => integer()();
+  IntColumn get illustNum => integer()();
+  IntColumn get talkingRowRate => integer()();
+  DateTimeColumn get updatedAt => dateTime()();
+}
+
+enum BigGenre {
+  love,
+  fantasy,
+  literature,
+  sciFiction,
+  other,
+  none,
+}
+
+enum Genre {
+  anotherWorldLove,
+  realWorldLove,
+  highFantasy,
+  lowFantasy,
+  pureLiterature,
+  drama,
+  history,
+  mystery,
+  horror,
+  action,
+  comedy,
+  virtualRealityGame,
+  universe,
+  sciFiction,
+  panic,
+  fairyTale,
+  poem,
+  essay,
+  replay,
+  other,
+  none,
+}
+
+enum WriterDevice {
+  phoneOnly,
+  laptopOnly,
+  laptopAndPhone,
+}
+
+class NovelDetailKeyWords extends Table {
+  IntColumn get novelDetail => integer()();
+  IntColumn get keyword => integer()();
 }
 
 // LazyDatabase _openConnection() {
@@ -71,14 +145,23 @@ class Reminders extends Table {
 //   });
 // }
 
-@UseMoor(tables: [Todos, Categories, Schedules, Reminders])
-class EnhanTodoDatabase {
-// class EnhanTodoDatabase extends _$EnhanTodoDatabase {
-  // EnhanTodoDatabase._internal() : super(_openConnection());
+@UseMoor(tables: [
+  Writers,
+  Novels,
+  Chapters,
+  Episodes,
+  Illusts,
+  KeyWords,
+  NovelDetails,
+  NovelDetailKeyWords
+])
+class NarouPaperDatabase {
+// class NarouPaperDatabase extends _$NarouPaperDatabase {
+  // NarouPaperDatabase._internal() : super(_openConnection());
   //
-  // factory EnhanTodoDatabase() => _enhanTodoDatabase;
+  // factory NarouPaperDatabase() => _narouPaperDatabase;
   //
-  // static final _enhanTodoDatabase = EnhanTodoDatabase._internal();
+  // static final _narouPaperDatabase = NarouPaperDatabase._internal();
   //
   // @override
   // int get schemaVersion => 1;
